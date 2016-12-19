853ee7a99e80b0e0c43bd53e35f2a4c08a5b6251
kelong <ke.long@tcl.com>
2016-12-19 19:09:51 +0800

    redis

--- simple-service/src/main/java/com/boot/dao/redis/RedisConfig.java
+++ simple-service/src/main/java/com/boot/dao/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.boot.config.redis;
+package com.boot.dao.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- simple-service/src/main/java/com/boot/dao/redis/RedisObjectSerializer.java
+++ simple-service/src/main/java/com/boot/dao/redis/RedisObjectSerializer.java
@@ -1,4 +1,4 @@
-package com.boot.config.redis;
+package com.boot.dao.redis;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.serializer.support.DeserializingConverter;
