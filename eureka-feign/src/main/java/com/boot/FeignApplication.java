660e5978345256ed1dc546650bf5f8eae41c23bb
longke <kelong_xhu@126.com>
2017-03-19 22:54:21 +0800

    config

--- eureka-feign/src/main/java/com/boot/FeignApplication.java
+++ eureka-feign/src/main/java/com/boot/FeignApplication.java
@@ -1,27 +1,14 @@
 package com.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
-@Configuration
-@ComponentScan("com.boot")
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class FeignApplication {
-
-	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeignApplication.class, args);
--- eureka-feign/src/main/java/com/boot/web/ConsumerController.java
+++ eureka-feign/src/main/java/com/boot/web/ConsumerController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
@@ -16,14 +15,5 @@ public class ConsumerController {
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add() {
         return computeClient.add(10, 20);
-    }
-
-
-    @Autowired
-    RestTemplate restTemplate;
-
-    @RequestMapping(value = "/add2", method = RequestMethod.GET)
-    public String add2() {
-        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
     }
 }
